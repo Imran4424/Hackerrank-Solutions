798d12d98c21875fc081e72a09f070884eb007e9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-13 16:12:06 +0600

    C++

---
+++ C++/8.ArraysIntroduction.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+
+    int n;
+    cin >> n;
+
+    vector<int> ar(n);
+
+    for(int i=0; i<ar.size(); i++)
+    {
+        cin >> ar[i];
+    }
+
+    for(int i=ar.size()-1; i>=0; i--)
+    {
+        cout << ar[i] << " ";
+    }
+    return 0;
+}
