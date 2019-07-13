d1f3eaf9057859cd3ead27ef650fef4faad9af6e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-13 15:56:50 +0600

    C++

--- C++/3.BasicDataType.cpp
+++ C++/3.BasicDataType.cpp
@@ -14,12 +14,18 @@ int main()
 
     cin >> a >> b >> c >> d;
 
-    cout << a << endl; 
-    cout << b << endl;
-    cout << c << endl;
+    // cout << a << endl; 
+    // cout << b << endl;
+    // cout << c << endl;
 
-    cout << fixed << setprecision(3) << d << endl;
-    cout << fixed << setprecision(9) << e << endl;
+    // cout << fixed << setprecision(3) << d << endl;
+    // cout << fixed << setprecision(9) << e << endl;
+
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    printf("%lf\n", e);
 
 
     return 0;
