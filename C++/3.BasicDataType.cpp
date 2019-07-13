1cc04ff2419142fb5ff94244081ee8a5307ee218
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-13 15:39:09 +0600

    C++

---
+++ C++/3.BasicDataType.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Complete the code.
+
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    printf("%lf\n", e);
+
+    return 0;
+}
+
