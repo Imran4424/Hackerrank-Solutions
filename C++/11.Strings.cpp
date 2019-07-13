015aa465558638828d3eb3454c507aaec107be13
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-13 16:18:32 +0600

    C++

---
+++ C++/11.Strings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	// Complete the program
+
+    string first, second;
+
+    cin >> first >> second;
+
+    cout << first.size() << " " << second.size() << endl;
+    cout << first + second << endl;
+
+    swap(first[0],second[0]);
+
+    cout << first << " " << second << endl;
+   
+    return 0;
+}
+
