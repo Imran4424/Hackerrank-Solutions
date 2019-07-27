b3d028c6106e6c086c2b60e0832b414a19bdf680
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-27 16:43:50 +0600

    c++

---
+++ Data Structure/1.Arrays.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+typedef long long int lli;
+
+int main()
+{
+    lli siz;
+    cin>>siz;
+
+    lli ar[siz];
+
+    for(lli i=0;i<siz;i++)
+    {
+        cin>>ar[i];
+    }
+
+    for(lli i=siz-1;i>0;i--)
+    {
+        cout<<ar[i]<<" ";
+    }
+
+    cout<<ar[0]<<endl;
+
+    return 0;
+}
