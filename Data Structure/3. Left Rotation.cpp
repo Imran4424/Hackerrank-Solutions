594ec711fdc970f21fda203bdeff6c8c9127b5a9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-27 17:03:15 +0600

    c++

---
+++ Data Structure/3. Left Rotation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int size, rotations;
+
+	cin >> size >> rotations;
+
+	vector<int> arr;
+
+	fo
+
+	return 0;
+}
