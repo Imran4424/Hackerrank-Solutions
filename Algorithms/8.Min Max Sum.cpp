684b681fe41dc975330f5dbfefdec86e0659fb51
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-28 19:20:45 +0600

    algorithm

--- Algorithms/8.Min Max Sum.cpp
+++ Algorithms/8.Min Max Sum.cpp
@@ -6,8 +6,17 @@ typedef long long int lli;
 
 int main(int argc, char const *argv[])
 {
-	
-	
+	vector<lli> arr;
+
+	lli pot;
+
+	for (int i = 0; i < 5; ++i)
+	{
+		cin >> pot;
+
+		arr.push_back(pot);
+	}
+
 
 	return 0;
 }
