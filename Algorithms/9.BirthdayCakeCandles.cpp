11bef4585200785314fb37cc91c88657ebea6b2c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-28 21:08:07 +0600

    algorithm

--- Algorithms/9.BirthdayCakeCandles.cpp
+++ Algorithms/9.BirthdayCakeCandles.cpp
@@ -11,7 +11,7 @@ int birthdayCakeCandles(vector<int> ar)
 
     int max = ar[0];
 
-    for (int i = 0; i < ar.size(); ++i)
+    for (int i = 1; i < ar.size(); ++i)
     {
         /* code */
     }
