6d0f5dc83df334b58571ee85ec180266a96f67ed
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-28 09:38:47 +0600

    algorithm

--- Algorithms/2.Simple Array Sum.cpp
+++ Algorithms/2.Simple Array Sum.cpp
@@ -10,10 +10,10 @@ int main()
 {
 	int n, sum = 0;
 	cin >> n;
-	
+
 	vector<int> arr(n);
 
-	for(int arr_i = 0;arr_i < n;arr_i++)
+	for(int arr_i = 0; arr_i < n; arr_i++)
 	{
 		cin >> arr[arr_i];
 		sum += arr[arr_i];
