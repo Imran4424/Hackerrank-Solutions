f9a80a8aca498f580fc379cffd7145e17e99a267
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-28 07:26:22 +0600

    algorithm

--- Algorithms/Plus Minus.cpp
+++ Algorithms/Plus Minus.cpp
@@ -8,7 +8,25 @@ int main(int argc, char const *argv[])
 
 	int positiveCount = 0, negativeCount = 0, zeroCount = 0;
 
-	
+	int bucket;
+
+	for (int i = 0; i < size; ++i)
+	{
+		cin >> bucket;
+
+		if (bucket > 0)
+		{
+			positiveCount++;
+		}
+		else if(bucket < 0)
+		{
+			negativeCount++;
+		}
+		else
+		{
+			zeroCount++;
+		}
+	}
 
 	return 0;
 }
