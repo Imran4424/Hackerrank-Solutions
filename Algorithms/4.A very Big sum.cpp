67af636a26fb1283b1d25f5a226ff169aef7eed9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-28 17:21:39 +0600

    algorithm

--- Algorithms/4.A very Big sum.cpp
+++ Algorithms/4.A very Big sum.cpp
@@ -7,7 +7,12 @@ vector<string> split_string(string);
 // Complete the aVeryBigSum function below.
 long aVeryBigSum(vector<long> ar) 
 {
-    
+    long sum = 0;
+
+    for (int i = 0; i < ar.size(); ++i)
+    {
+        /* code */
+    }
 
 }
 
