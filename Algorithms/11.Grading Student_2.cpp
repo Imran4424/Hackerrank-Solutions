fa436638da3010a2701814b663f78c843519a179
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-30 09:42:43 +0600

    algorithm

--- Algorithms/11.Grading Student_2.cpp
+++ Algorithms/11.Grading Student_2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 
@@ -288,26 +288,3 @@ int main()
     return 0;
 }
 
-string ltrim(const string &str) 
-{
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) 
-{
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
