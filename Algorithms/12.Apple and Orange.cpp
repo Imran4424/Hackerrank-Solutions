170923100387597d503752fbee51fd83e03f0503
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-30 14:24:58 +0600

    algorithm

--- Algorithms/12.Apple and Orange.cpp
+++ Algorithms/12.Apple and Orange.cpp
@@ -17,6 +17,14 @@ void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vecto
         }
     }
 
+    for (int i = 0; i < apples.size(); ++i)
+    {
+        if (a + apples[i] >= s && a + apples[i] <= t)
+        {
+            applesCount++;
+        }
+    }
+
 }
 
 int main()
