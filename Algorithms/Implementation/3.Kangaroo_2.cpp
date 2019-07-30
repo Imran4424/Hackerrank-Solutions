fdce89a47683b2300a681d1d66a6feba8a50cd44
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-30 18:23:03 +0600

    algorithm

--- Algorithms/Implementation/3.Kangaroo_2.cpp
+++ Algorithms/Implementation/3.Kangaroo_2.cpp
@@ -17,6 +17,14 @@ string kangaroo(int x1, int v1, int x2, int v2)
 		return "NO";
 	}
 
+    if (v1 == v2)
+    {
+        if (x1 > x2 || x1 < x2)
+        {
+            return "NO";
+        }
+    }
+
     if (x1 == x2)
     {
         return "YES";
