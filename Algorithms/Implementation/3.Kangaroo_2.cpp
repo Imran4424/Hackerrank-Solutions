82d3e6b5f3109fc4185348e1280edbc14fab8ac3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-30 18:20:08 +0600

    algorithm

--- Algorithms/Implementation/3.Kangaroo_2.cpp
+++ Algorithms/Implementation/3.Kangaroo_2.cpp
@@ -17,11 +17,19 @@ string kangaroo(int x1, int v1, int x2, int v2)
 		return "NO";
 	}
 
+    if (x1 == x2)
+    {
+        return "YES";
+    }
 
-    //int indicator = abs(x1 - x2) % abs(v2 - v1);	
+    int indicator = abs(x1 - x2) % abs(v2 - v1);	
 
+    if (0 == indicator)
+    {
+        return "YES";
+    }
 
-	return "YES";
+	return "NO";
 }
 
 int main()
