c3cf9bd0979863cf433a876042c88a5e3345c10d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-30 23:16:11 +0600

    algorithm

--- Algorithms/Implementation/6.Birthday Chocolate.cpp
+++ Algorithms/Implementation/6.Birthday Chocolate.cpp
@@ -13,9 +13,21 @@ int birthday(vector<int> s, int d, int m)
 
     for (int i = 0; i < s.size()-1; ++i)
     {
-        
+        int sum = 0;
+
+        for(int j = i; j < i + m && j < s.size(); j++)
+        {
+            sum += s[j];
+        }
+
+        if (sum == d)
+        {
+            count++;
+        }
     }
 
+
+    return 
 }
 
 int main()
