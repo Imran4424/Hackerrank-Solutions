50d75eb84b259e5a2c1d3cff6bd56fd89df6dcdc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-07-31 10:43:48 +0600

    algorithm

--- Algorithms/Implementation/7.Divisible Sum Pairs.cpp
+++ Algorithms/Implementation/7.Divisible Sum Pairs.cpp
@@ -7,7 +7,22 @@ vector<string> split_string(string);
 // Complete the divisibleSumPairs function below.
 int divisibleSumPairs(int n, int k, vector<int> ar) 
 {
-    
+    int count = 0;
+
+    for (int i = 0; i < s.size(); ++i)
+    {
+        int sum = 0;
+
+        for(int j = i; j < i + m && j < s.size(); j++)
+        {
+            sum += s[j];
+        }
+
+        if (sum == d)
+        {
+            count++;
+        }
+    }
 
 }
 
