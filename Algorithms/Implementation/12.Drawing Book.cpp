66403a4601d80da165ec282e62dd95812cfc1f15
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-01 23:35:08 +0600

    best programmer

--- Algorithms/Implementation/12.Drawing Book.cpp
+++ Algorithms/Implementation/12.Drawing Book.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 {
 	while(1)
 	{
-		cout << "I wanna be the best programmer in the world";
+		cout << "I wanna be the best programmer in the world" << endl;
 	}
 
 	return 0;
