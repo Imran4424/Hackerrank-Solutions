82dea79983ff7088da6bc6a3d8ee27fc861ee673
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-01 23:53:28 +0600

    hackerrank implementation

--- Algorithms/Implementation/12.Drawing Book.cpp
+++ Algorithms/Implementation/12.Drawing Book.cpp
@@ -5,15 +5,6 @@ int main(int argc, char const *argv[])
 {
 	while(1)
 	{
-		cout << "I wanna be the best programmer in the world" << endl;
-
-		cout << "I wanna help the society" << endl;
-
-		cout << "I always try to be a honest man" << endl;
-
-		cout << "The first thing my father taught me is, Honesty is the best policy" << endl;		
-		
-		cout << "I want to stay honest until my last breath" << endl;
 	}
 
 	return 0;
