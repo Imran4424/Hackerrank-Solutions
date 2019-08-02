f2a5a569c1cdac4b54213eefaa7364f59b8455fa
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-02 14:27:23 +0600

    hackerrank implementation

--- Algorithms/Implementation/14.Electronics Shop.cpp
+++ Algorithms/Implementation/14.Electronics Shop.cpp
@@ -15,7 +15,10 @@ int getMoneySpent(vector<int> keyboards, vector<int> drives, int b)
     {
         for (int j = 0; j < drives.size(); ++j)
         {
-            /* code */
+            if(keyboards[i] + drives[j] > max && keyboards[i] + drives[j] <= b)
+            {
+                max = keyboards[i] + drives[j];
+            }
         }
     }
 
