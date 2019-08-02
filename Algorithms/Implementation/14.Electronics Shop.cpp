75a80fb6811aea7ab6bfe4d5e8ffd07f43ef5908
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-02 14:20:43 +0600

    hackerrank implementation

--- Algorithms/Implementation/14.Electronics Shop.cpp
+++ Algorithms/Implementation/14.Electronics Shop.cpp
@@ -68,14 +68,16 @@ int main()
     return 0;
 }
 
-vector<string> split_string(string input_string) {
+vector<string> split_string(string input_string) 
+{
     string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
         return x == y and x == ' ';
     });
 
     input_string.erase(new_end, input_string.end());
 
-    while (input_string[input_string.length() - 1] == ' ') {
+    while (input_string[input_string.length() - 1] == ' ') 
+    {
         input_string.pop_back();
     }
 
@@ -85,7 +87,8 @@ vector<string> split_string(string input_string) {
     size_t i = 0;
     size_t pos = input_string.find(delimiter);
 
-    while (pos != string::npos) {
+    while (pos != string::npos) 
+    {
         splits.push_back(input_string.substr(i, pos - i));
 
         i = pos + 1;
