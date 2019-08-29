8233806ec22edcde40ef80121fd3a1f0a9f7475e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-29 12:56:06 +0600

    graph

--- Algorithms/Graph/2.RoadsAndLibraries.cpp
+++ Algorithms/Graph/2.RoadsAndLibraries.cpp
@@ -3,37 +3,42 @@ using namespace std;
 
 typedef long long int lli;
 
-vector <int> *adjacency;
+vector <lli> *adjacency;
 
-void AddEdge(int u, int v) // unidirected
+void AddEdge(lli u, lli v) // unidirected
 {
 	adjacency[u].push_back(v);
 	adjacency[v].push_back(u);
 }
 
-
+lli FindMinimumCost(lli cities, lli libraryCost, lli roadCost)
+{
+	
+}
 
 
 int main(int argc, char const *argv[])
 {
-	int test;
+	lli test;
 	cin >> test;
 
 	while(test--)
 	{
-		int cities, roads, libraryCost, roadCost;
+		lli cities, roads, libraryCost, roadCost;
 		cin >> cities >> roads >> libraryCost >> roadCost;
 
-		adjacency = new vector <int> [cities+1];
+		adjacency = new vector <lli> [cities+1];
 
-		int u, v;
+		lli u, v;
 
-		for (int i = 0; i < roads; ++i)
+		for (lli i = 0; i < roads; ++i)
 		{
 			cin >> u >> v;
 
 			AddEdge(u, v);
 		}
+
+
 	}
 	
 	return 0;
