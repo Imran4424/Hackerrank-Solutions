415e07082ffc4f9b20de8219c859f3c8ea581339
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-31 20:34:01 +0600

    graph

---
+++ Algorithms/Graph/3.JourneyToTheMoonDisjoint.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the journeyToMoon function below.
+lli journeyToMoon(lli n, vector<vector<lli>> astronaut) 
+{
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string np_temp;
+    getline(cin, np_temp);
+
+    vector<string> np = split_string(np_temp);
+
+    lli n = stoi(np[0]);
+
+    lli p = stoi(np[1]);
+
+    vector<vector<lli>> astronaut(p);
+    for (lli i = 0; i < p; i++) {
+        astronaut[i].resize(2);
+
+        for (lli j = 0; j < 2; j++) {
+            cin >> astronaut[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    lli result = journeyToMoon(n, astronaut);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
