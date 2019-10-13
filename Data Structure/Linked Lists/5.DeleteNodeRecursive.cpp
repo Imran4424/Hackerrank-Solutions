1204bac131b3c751d1c6e5bc0631e405b6f7e670
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-13 18:00:03 +0600

    data structure

--- Data Structure/Linked Lists/5.DeleteNodeRecursive.cpp
+++ Data Structure/Linked Lists/5.DeleteNodeRecursive.cpp
@@ -2,28 +2,33 @@
 
 using namespace std;
 
-class SinglyLinkedListNode {
+class SinglyLinkedListNode 
+{
     public:
         int data;
         SinglyLinkedListNode *next;
 
-        SinglyLinkedListNode(int node_data) {
+        SinglyLinkedListNode(int node_data) 
+        {
             this->data = node_data;
             this->next = nullptr;
         }
 };
 
-class SinglyLinkedList {
+class SinglyLinkedList 
+{
     public:
         SinglyLinkedListNode *head;
         SinglyLinkedListNode *tail;
 
-        SinglyLinkedList() {
+        SinglyLinkedList() 
+        {
             this->head = nullptr;
             this->tail = nullptr;
         }
 
-        void insert_node(int node_data) {
+        void insert_node(int node_data) 
+        {
             SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
 
             if (!this->head) {
@@ -36,7 +41,8 @@ class SinglyLinkedList {
         }
 };
 
-void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) 
+{
     while (node) {
         fout << node->data;
 
@@ -48,7 +54,8 @@ void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& 
     }
 }
 
-void free_singly_linked_list(SinglyLinkedListNode* node) {
+void free_singly_linked_list(SinglyLinkedListNode* node) 
+{
     while (node) {
         SinglyLinkedListNode* temp = node;
         node = node->next;
