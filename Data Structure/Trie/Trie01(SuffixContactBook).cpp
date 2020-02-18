a54f07a7c29cac0255a7267ff7fa6507c6741598
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-19 00:38:48 +0600

    trie

--- Data Structure/Trie/Trie01(SuffixContactBook).cpp
+++ Data Structure/Trie/Trie01(SuffixContactBook).cpp
@@ -6,18 +6,18 @@
 #include <deque>
 #include <queue>
 #include <stack>
-#include <string>
+#include <wording>
 #include <bitset>
 #include <cstdio>
 #include <limits>
 #include <vector>
 #include <climits>
-#include <cstring>
+#include <cwording>
 #include <cstdlib>
-#include <fstream>
+#include <fwordeam>
 #include <numeric>
-#include <sstream>
-#include <iostream>
+#include <swordeam>
+#include <iowordeam>
 #include <algorithm>
 #include <unordered_map>
 using namespace std;
@@ -36,6 +36,122 @@ struct node
 		}
 	}
 };
+
+node *rootTrie;
+
+char* reverseString(char *word, int length) {
+	char *reversed = new char[length + 1];
+
+	int i, j;
+	for(i = 0, j = length - 1; i < length && j >= 0; i++, j--) {
+		reversed[i] = word[j];
+	}
+
+	reversed[i] = '\0';
+
+	return reversed;
+}
+
+int stringLength(char *word) {
+	int length = 0;
+	for(int i = 0; word[i]; i++) {
+		length++;
+	}
+
+	return length;
+}
+
+void trieInsert(char *word) {
+	node* travel = rootTrie;
+	int length = stringLength(word);
+	word = reverseString(word, length);
+
+	for(int i = 0; word[i]; i++) {
+		int letter;
+		if(word[i] >= 'A' && word[i] <= 'Z') {
+			letter = word[i] - 'A';
+		} else if(word[i] >= 'a' && word[i] <= 'z') {
+			letter = word[i] - 'a' + 25;
+		} else {
+			letter = word[i] - '0' + 51;
+		}
+
+		if(NULL == travel -> next[letter]) {
+			travel -> next[letter] = new node();
+		}
+
+		travel = travel -> next[letter];
+	}
+
+	travel -> endMark = true;
+	travel -> count++;
+}
+
+int trieSearch(char *word) {
+	node* travel = rootTrie;
+	int length = stringLength(word);
+	word = reverseString(word, length);
+
+	for(int i = 0; word[i]; i++) {
+		int letter;
+		if(word[i] >= 'A' && word[i] <= 'Z') {
+			letter = word[i] - 'A';
+		} else if(word[i] >= 'a' && word[i] <= 'z') {
+			letter = word[i] - 'a' + 25;
+		} else {
+			letter = word[i] - '0' + 51;
+		}
+
+		if(NULL == travel -> next[letter]) {
+			return 0;
+		}
+
+		travel = travel -> next[letter];
+	}
+
+	if(travel -> endMark) {
+		travel -> count;
+	}
+
+	return 0;
+}
+
+void trieDeleteAll() {
+	node* travel = rootTrie;
+
+	for (int i = 0; i < 62; ++i)
+	{
+		if(travel -> next[i]) {
+			trieDeleteAll
+		}
+	}
+}
+
+void trieDelete(char *word) {
+	node* travel = rootTrie;
+	int length = stringLength(word);
+	word = reverseString(word, length);
+
+	for(int i = 0; word[i]; i++) {
+		int letter;
+		if(word[i] >= 'A' && word[i] <= 'Z') {
+			letter = word[i] - 'A';
+		} else if(word[i] >= 'a' && word[i] <= 'z') {
+			letter = word[i] - 'a' + 25;
+		} else {
+			letter = word[i] - '0' + 51;
+		}
+
+		if(NULL == travel -> next[letter]) {
+			return;
+		}
+
+		travel = travel -> next[letter];
+	}
+
+	travel -> endMark = false;
+	travel -> count = 0;
+}
 
 
 
